db0ec40bebffdc2b8660d15fdeb0efa47b92a63a
frrst-ian <frrstxd@proton.me>
2026-03-07 19:44:20 +0800

    feat: get course by ID

--- packages/server/controllers/courseController.js
+++ packages/server/controllers/courseController.js
@@ -5,6 +5,11 @@ async function getAllCourses(req, res) {
     return res.json(courses);
 }
 
-async function getCourseById(req, res) {}
+async function getCourseById(req, res) {
+    const id = parseInt(req.params.id);
+
+    const course = await db.getCourseById(id);
+    return res.json(course);
+}
 
 module.exports = { getAllCourses, getCourseById };
--- packages/server/routes/courseRouter.js
+++ packages/server/routes/courseRouter.js
@@ -1,9 +1,23 @@
 const { Router } = require("express");
 const courseRouter = Router();
-const { getAllCourses } = require("../controllers/courseController");
+const {
+    getAllCourses,
+    getCourseById,
+} = require("../controllers/courseController");
 const authenticateJwt = require("../middleware/auth");
 const requireRole = require("../middleware/requireRole");
 
-courseRouter.get("/", authenticateJwt, requireRole("student", "teacher"), getAllCourses);
+courseRouter.get(
+    "/",
+    authenticateJwt,
+    requireRole("student", "teacher"),
+    getAllCourses,
+);
+courseRouter.get(
+    "/:id",
+    authenticateJwt,
+    requireRole("student", "teacher"),
+    getCourseById,
+);
 
 module.exports = courseRouter;
