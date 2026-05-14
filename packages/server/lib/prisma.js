d4e6adb46bcb02419c9265b23374ff14499e399f
frrst-ian <frrstxd@proton.me>
2026-05-14 17:23:35 +0800

    fix: use prisma adapter

--- packages/server/lib/prisma.js
+++ packages/server/lib/prisma.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
-const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("../generated/prisma/client.js");
 
-const prisma = new PrismaClient({
-  datasourceUrl: `${process.env.DATABASE_URL}`,
-  log: ["error", "warn"],
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({
+  connectionString,
+  ssl: { rejectUnauthorized: false },
 });
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
